4f516453e719426aa6bb17a9ff180afdb143ff21
Alden Garcia <76920905+rampus-bit@users.noreply.github.com>
2021-05-24 20:55:25 -0400

    Add files via upload

--- cs.sh
+++ cs.sh
@@ -1,29 +1,56 @@
 #!/bin/bash
 
+# Variables
+# i3CS=$(grep bindsym $HOME/.config/i3/config | awk {'print $2 " " $3 "--- " $4 $7'} | sed 's/exec//g' | sed 's/$mod/MOD /g' | column)
+# QtileCS=$(grep Key $HOME/.config/qtile/config.py | awk {'print $2 "" $3 "--- " $4 $7'} | column)
+# DWMCS=$(grep -s XK_ $Home/.config/suckless/dwm/config.def.h | awk {'print $2 $3 $4 $7'})
+
 # Colors
 RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+GRAY="\e[90m"
+WHITE="\e[97m"
+
 END="\e[0m"
 
 # i3 CS
-i3CS=$(grep bindsym $HOME/.config/i3/config | awk {'print $2 " " $3 "--- " $4 $7'} | sed 's/exec//g' | sed 's/$mod/MOD /g')
-echo $i3CS | column
-#grep bindsym $HOME/.config/i3/config | awk {'print $2 " " $3 "--- " $4 $7'} | sed 's/exec//g' | sed 's/$mod/MOD /g' | column
-
-# Qtile CS
-echo "Qtile Cheat-Sheet"
-# grep Key $HOME/.config/qtile/config.py | awk {'print $2 "" $3 "--- " $4 $7'}
-
-# DWM CS
-echo "DWM Cheat-Sheet"
-DWMCS = grep XK_ $Home/.config/suckless/dwm/config.def.h | awk {'print $2 $3 $4 $7'}
-
-if [[ -f "$HOME/.config/suckless/dwm/config.def.h" ]]
+echo "i3 Cheat-Sheet:"
+if [[ -f "$HOME/.config/i3/config" ]]
 then
-	echo "This file exists"
+	grep bindsym $HOME/.config/i3/config | awk {'print $2 " " $3 "--- " $4 $7'} | sed 's/exec//g' | sed 's/$mod/MOD /g' | column
 else
-	echo "This file does not exist"
+	echo "You seem to not have i3 installed"
 fi
 
-grep XK_ $HOME/.config/suckless/dwm/config.def.h | awk {'print $2 $3 $4 $7'}
+# Qtile CS
+echo
+echo "Qtile Cheat-Sheet:"
+if [[ -f "$HOME/.config/qtile/config.py" ]]
+then
+	grep bindsym $HOME/.config/i3/config | awk {'print $2 " " $3 "--- " $4 $7'} | sed 's/exec//g' | sed 's/$mod/MOD /g' | column
+else
+	echo "You seem to not have Qtile installed"
+fi
+echo
+
+# DWM CS
+echo "DWM Cheat-Sheet:"
+if [[ -f "$HOME/.config/suckless/dwm/config.def.h" ]]
+then
+	echo $DWMCS
+else
+	echo "You seem to not have DWM installed"
+fi
+
+echo
 
 echo -e "${RED}THIS IS SOME RED TEXT${END}"
+
+# Old Code
+# grep XK_ $HOME/.config/suckless/dwm/config.def.h | awk {'print $2 $3 $4 $7'}
+# grep bindsym $HOME/.config/i3/config | awk {'print $2 " " $3 "--- " $4 $7'} | sed 's/exec//g' | sed 's/$mod/MOD /g' | column
+# grep Key $HOME/.config/qtile/config.py | awk {'print $2 "" $3 "--- " $4 $7'}
