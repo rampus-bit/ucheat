b8345af28d6e2c767f4fa3ecb9024b3d13e508e6
Alden Garcia <76920905+rampus-bit@users.noreply.github.com>
2021-05-27 22:13:45 -0400

    Add files via upload

--- ucheat.sh
+++ ucheat.sh
@@ -20,38 +20,47 @@ LIGHTBLUE="\e[94m"
 END="\e[0m"
 
 # i3 CS
-echo -e "${LIGHTBLUE}i3 Cheat-Sheet:${END}"
-if [[ -f "$HOME/.config/i3/config" ]]
-then
-	grep bindsym $HOME/.config/i3/config | awk {'print $2 " " $3 " --- " $4 $7'} | sed 's/$mod/MOD/g' | sed 's/+/ + /g' | sed 's/exec//g' | column
-else
-	echo "You seem to not have i3 installed."
-fi
+i3_CS() {
+	echo -e "${LIGHTBLUE}i3 Cheat-Sheet:${END}"
+	if [[ -f "$HOME/.config/i3/config" ]]
+	then
+		grep bindsym $HOME/.config/i3/config | awk {'print $2 " " $3 " --- " $4 $7'} | sed 's/$mod/MOD/g' | sed 's/+/ + /g' | sed 's/exec//g' | column
+	else
+		echo "You seem to not have i3 installed."
+	fi
+}
+
+i3_CS
+
 
 # Qtile CS
-echo
-echo -e "${GREEN}Qtile Cheat-Sheet:${END}"
-if [[ -f "$HOME/.config/qtile/config.py" ]]
-then
-	grep Key $HOME/.config/qtile/config.py | awk {'print $2 "" $3 "--- " $4 $7'} | column
-else
-	echo "You seem to not have Qtile installed."
-fi
-echo
+qtile_CS() {
+	echo
+	echo -e "${GREEN}Qtile Cheat-Sheet:${END}"
+	if [[ -f "$HOME/.config/qtile/config.py" ]]
+	then
+		grep Key $HOME/.config/qtile/config.py | awk {'print $2 "" $3 "--- " $4 $7'} | column
+	else
+		echo "You seem to not have Qtile installed."
+	fi
+	echo
+}
 
-# DWM CS
-echo -e "${GRAY}DWM Cheat-Sheet:${END}"
-if [[ -f "$HOME/.config/suckless/dwm/config.def.h" ]]
-then
-	echo $DWMCS
-else
-	echo "You seem to not have DWM installed."
-fi
+qtile_CS
 
-echo
+#DWM CS
+DWM_CS() {
+	echo -e "${GRAY}DWM Cheat-Sheet:${END}"
+	if [[ -f "$HOME/.config/suckless/dwm/config.def.h" ]]
+	then
+		echo $DWMCS
+	else
+		echo "You seem to not have DWM installed."
+	fi
+}
 
-echo -e "${RED}THIS IS SOME RED TEXT${END}"
-echo -e "${GRAY}THIS IS SOME YELLOW TEXT${END}"
+DWM_CS
+
 
 # Old Code
 # grep XK_ $HOME/.config/suckless/dwm/config.def.h | awk {'print $2 $3 $4 $7'}
